b60431907e55edf1b54f322e7594b260f27c45d9
Matthew Simon <info@boxadesign.co.uk>
2022-01-26 10:46:17 +0000

    Initial work on navigation

--- src/prototypes/sub-navigation/index.js
+++ src/prototypes/sub-navigation/index.js
@@ -2,3 +2,5 @@
 Imports question manager, summary generator, answer piping and previous link history
 ************************************************************************************/
 import '../../helpers/index.js';
+
+import './components/header-navigation/header-nav.dom';
