d8bc79e0e2de5edb76627e6172dfe1e05436b10d
Matthew Simon <info@boxadesign.co.uk>
2022-02-17 12:24:50 +0000

    Add overview link and fixes

--- src/prototypes/example/index.js
+++
@@ -1,4 +0,0 @@
-/***********************************************************************************
-Imports question manager, summary generator, answer piping and previous link history
-************************************************************************************/
-import '../../helpers/index.js';
